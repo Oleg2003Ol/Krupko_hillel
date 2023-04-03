aa1d3cbd5640c5a4a12344b073f7c1ced13730ff
KrupkoOleh <oleg.krupko.2003@gmail.com>
2023-04-03 17:07:41 +0300

    add method about calculate price with Discount

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator
 from django.db import models
+from django.utils import timezone
 
 from project.constants import MAX_DIGITS, DECIMAL_PLACES
 from project.mixins.models import PKMixin
@@ -19,6 +20,17 @@ class Discount(models.Model):
         choices=DiscountTypes.choices,
         default=DiscountTypes.VALUE
     )
+    valid_until = models.DateTimeField(
+        null=True,
+        blank=True
+    )
+
+    @property
+    def is_valid(self):
+        is_valid = self.is_active
+        if self.valid_until:
+            is_valid &= timezone.now() <= self.valid_until
+        return is_valid
 
 
 class Order(PKMixin):
@@ -43,20 +55,38 @@ class Order(PKMixin):
     is_active = models.BooleanField(default=True)
     order_number = models.PositiveIntegerField(default=1)
 
+    def get_total_amount(self):
+        total_amount = 0
+        for item in self.order_items.iterator():
+            total_amount += item.price * item.quantity
+        if self.discount and self.Discount.is_valid:
+            if self.discount.discount_type == DiscountTypes.PERCENT:
+                total_amount -= (total_amount * (self.discount.amount / 100))
+            else:
+                total_amount -= self.discount
+        return total_amount
+
 
 class OrderItem(PKMixin):
     order = models.ForeignKey(
         Order,
         on_delete=models.SET_NULL,
         null=True,
-        blank=True
+        blank=True,
+        related_name="order_items"
     )
     is_active = models.BooleanField(default=True)
     product = models.ForeignKey(
-        ...
+        'products.Product',
+        on_delete=models.PROTECT,
+        related_name="order_items"
     )
     quantity = models.PositiveSmallIntegerField(default=1)
     price = models.DecimalField(
         validators=[MinValueValidator(0)],
         max_digits=MAX_DIGITS,
-        decimal_places=DECIMAL_PLACES)
+        decimal_places=DECIMAL_PLACES
+    )
+
+    class Meta:
+        unique_together = ('order', 'product')

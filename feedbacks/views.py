874a96138ec49b59831a96bb72203fb62c1602d7
KrupkoOleh <oleg.krupko.2003@gmail.com>
2023-04-06 19:59:38 +0300

    edit add feedbacks

--- feedbacks/views.py
+++ feedbacks/views.py
@@ -8,17 +8,18 @@ from feedbacks.models import Feedback
 
 def feedbacks(request):
     form = FeedbackModelForm(request.user)
+    user = request.user
     if request.method == 'POST':
         form = FeedbackModelForm(user=request.user, data=request.POST)
-        if form.is_valid():
-            review = form.save(commit=False)
-            review.text = re.sub(r'[^\w\s]', '', review.text)
-            review.text = re.sub(r'<.*?>', '', review.text)
-            review.user = request.user
-            review.save()
+        if not user.is_auntificated:
+            if form.is_valid():
+                review = form.save(commit=False)
+                review.text = re.sub(r'[^\w\s]', '', review.text)
+                review.text = re.sub(r'<.*?>', '', review.text)
+                review.user = request.user
+                review.save()
     feedback = Feedback.objects.iterator()
     return render(request, 'feedbacks/index.html', context={
         'feedbacks': feedback,
         'form': form
     })
-

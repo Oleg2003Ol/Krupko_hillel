742f59373a0e478f73ce7b85e1f3bffd3eee0d2e
KrupkoOleh <oleg.krupko.2003@gmail.com>
2023-04-17 19:22:20 +0300

    bootstrap

--- products/views.py
+++ products/views.py
@@ -1,4 +1,3 @@
-from django.urls import reverse
 from django.views.generic import ListView
 from .models import Product
 
@@ -7,4 +6,3 @@ class ProductsListView(ListView):
     model = Product
     template_name = 'products/index.html'
     context_object_name = 'products'
-

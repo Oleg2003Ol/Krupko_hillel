13127b3d64eb6c76322489cefd31514fa807fff4
KrupkoOleh <oleg.krupko.2003@gmail.com>
2023-04-17 19:19:50 +0300

    bootstrap

--- products/views.py
+++ products/views.py
@@ -8,7 +8,3 @@ class ProductsListView(ListView):
     template_name = 'products/index.html'
     context_object_name = 'products'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['products_url'] = reverse('products')
-        return context

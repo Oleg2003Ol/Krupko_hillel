f0b4085603fb436282265e2e69f59d273643f0bd
KrupkoOleh <oleg.krupko.2003@gmail.com>
2023-04-23 18:57:06 +0300

    export csv

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import ProductsListView
+from .views import ProductsListView, ExportCSVView
 
 urlpatterns = [
     path('', ProductsListView.as_view(), name='products'),
+    path('export-csv/', ExportCSVView.as_view(), name='export_to_csv')
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,8 @@
+import csv
 from django.views.generic import ListView
+from .models import Product
+from django.http import HttpResponse
+from django.views import View
 from .models import Product
 
 
@@ -6,3 +10,29 @@ class ProductsListView(ListView):
     model = Product
     template_name = 'products/index.html'
     context_object_name = 'products'
+
+
+class ExportCSVView(View):
+    template_name = 'products/export_to_pdf.html'
+
+    def get(self, request, *args, **kwargs):
+        headers = {
+            'Content-Type': 'text/csv',
+            'Content-Disposition': 'attachment; filename="products.csv"'
+        }
+        response = HttpResponse(headers=headers)
+        fields_name = ['name', 'description', 'sku', 'image', 'price', 'is_active']
+        writer = csv.DictWriter(response, fieldnames=fields_name, delimiter=';')
+        writer.writeheader()
+        for product in Product.objects.iterator():
+            writer.writerow(
+                {
+                    'name': product.name,
+                    'description': product.description,
+                    'image': product.image.name if product.image else 'no image',
+                    'sku': product.sku,
+                    'price': product.price,
+                    'is_active': product.is_active
+                }
+            )
+        return response

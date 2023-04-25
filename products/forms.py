50da3e882c11ca68ce9bb81aee4b13712621bad3
KrupkoOleh <oleg.krupko.2003@gmail.com>
2023-04-25 20:22:11 +0300

    update import csv

--- products/forms.py
+++ products/forms.py
@@ -4,12 +4,15 @@ from io import StringIO
 import csv
 from django import forms
 from django.core.exceptions import ValidationError
+from django.core.validators import FileExtensionValidator
 
 from products.models import Product, Category
 
 
 class ImportCSVForm(forms.Form):
-    file = forms.FileField()
+    file = forms.FileField(
+        validators=[FileExtensionValidator(['csv'])]
+    )
 
     def clean_file(self):
         csv_file = self.cleaned_data['file']
@@ -24,19 +27,21 @@ class ImportCSVForm(forms.Form):
                 except Category.DoesNotExist:
                     category = Category.objects.create(name=product['categories'])
                 try:
-                    new_product = Product.objects.create(
+                    new_product, created = Product.objects.get_or_create(
                         name=product['name'],
                         description=product['description'],
                         price=decimal.Decimal(product['price']),
                         sku=product['sku'],
                         is_active=product['is_active'],
                     )
-                    new_product.categories.add(category)
-                    product_list.append(new_product)
+                    if created:
+                        new_product.categories.add(category)
+                        product_list.append(new_product)
                 except (KeyError, decimal.InvalidOperation) as errors:
                     raise ValidationError(errors)
         return product_list
 
     def save(self):
         product_list = self.cleaned_data['file']
-        Product.objects.bulk_create(product_list)
+        for product in product_list:
+            product.save()
